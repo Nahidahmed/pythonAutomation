4524f2a399a17ed21b3e5c3737d94e973060f108
Nahidahmed <nahid.mansuri@gmail.com>
2021-01-01 22:15:32 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,9 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
 driver = webdriver.Chrome()
-driver.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+driver.maximize_window()
 
-messageField = driver.find_element_by_xpath('//*[@id="user-message"]')
-messageField.send_keys('Hello World')
 
-showMessageBtn = driver.find_element_by_xpath('//*[@id="get-input"]/button')
-showMessageBtn.click()
+def automate_web_form():
+    driver.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+    messageField = driver.find_element_by_xpath('//*[@id="user-message"]')
+    messageField.send_keys('Hello World')
+
+    showMessageBtn = driver.find_element_by_xpath('//*[@id="get-input"]/button')
+    showMessageBtn.click()
+
+
+def automate_drag_n_drop():
+    driver.get('http://dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
+    source = driver.find_element_by_xpath('//*[@id="box3"]')
+    destination = driver.find_element_by_xpath('//*[@id="box103"]')
+    actions = ActionChains(driver)
+    actions.drag_and_drop(source,destination).perform()
+
+automate_drag_n_drop()
